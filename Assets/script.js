724663b5493ec4160500ab78cbc3c44598f2f56e
Vickyb94 <oluwabusayo.arowosafe@gmail.com>
2022-11-24 16:00:36 -0400

    cleaned up code in index.html,script.js and style.css

--- Assets/script.js
+++ Assets/script.js
@@ -1,53 +1,55 @@
 
+
 //used the ready() method to ensure that the javascript is executed after loading the html file
 $(document).ready(function () {
-  //Added a listener for click events on the save button.
+   var saveBtn = $(".saveBtn");
+   //Added a listener for click events on the save button.
   $(".saveBtn").on("click", function () {
-    console.log(this); //'this' is the element that invokes the event in the event handler.
-    //getting the id attribute of the parent element
-    var text = $(this).siblings(".plan").val()
-    var time = $(this).parent().attr("id");
+      console.log(this); //'this' is the element that invokes the event in the event handler.
+      //created var plan and time to obtain storage values and to store the plan in the local storage"
+      var plan = $(this).siblings(".plan").val();
+      var time = $(this).parent().attr("id");
 
-    //to store the plan in the local storage,
-    localStorage.setItem(text, time);
-  });
-  //TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-    
-   //set function to get present time now
-  function timeBlock() {
-      var time = moment().hour();
-    }
-    $(".time-block").each(function() {
-      var currentTime = parseInt($(this).attr("id"));
-      console.log( currentTime, time)
-      //created a for loop to loop over the time blocks
-      //if time is less than currentTime, then it is in the past 
-      if ( time < currentTime) {
-        $(this).addClass("past");
-      //if time is strictly equal to current time, then it is the present
-    } else if ( time === currentTime) {
-        $(this).addClass("present");
-    } else {
-        $(this).addClass("future");
-    }
-  });
-  
+      //use local.storage(getitem) to retrieve saved plans from local storage
+      localStorage.setItem(time, plan);
+  })
+  //  //getting saved data from local storage; it helps to use id to save the data so it makes it easier to load
+      $("#9 .plan").val(localStorage.getItem("9"));
+      $("#10 .plan").val(localStorage.getItem("10"));
+      $("#11 .plan").val(localStorage.getItem("11"));
+      $("#12 .plan").val(localStorage.getItem("12"));
+      $("#1 .plan").val(localStorage.getItem("1"));
+      $("#2 .plan").val(localStorage.getItem("2"));
+      $("#3 .plan").val(localStorage.getItem("3"));
+      $("#4 .plan").val(localStorage.getItem("4"));
+      $("#5 .plan").val(localStorage.getItem("5"));
 
-  //getting saved data from local storage; it helps to use id to save the data so it makes it easier to load
-  $("#9 .plan").val(localStorage.getItem("9"));
-  $("#10 .plan").val(localStorage.getItem("10"));
-  $("#11 .plan").val(localStorage.getItem("11"));
-  $("#12 .plan").val(localStorage.getItem("12"));
-  $("#1 .plan").val(localStorage.getItem("1"));
-  $("#2 .plan").val(localStorage.getItem("2"));
-  $("#3 .plan").val(localStorage.getItem("3"));
-  $("#4 .plan").val(localStorage.getItem("4"));
-  $("#5 .plan").val(localStorage.getItem("5"));
 
-  //display current date and time in the header of the page
-  $("#currentDay").text(moment().format('dddd MMMM DD YYYY h:mm:ss a'));
+ //created a function to give current time when called
+  function timeBlocker() {
+      var timeNow = moment().hour();
+      // created a for loop to set conditions for past, present, future time
+      $(".time-block").each(function () {
+          var hour = parseInt($(this).attr("id").split("hour")[1]);
+          console.log( hour, timeNow)
+
+          //sets condition for output when hour is measured against timeNow
+          if (hour < timeNow) {
+              $(this).addClass("past");
+          }
+
+          else if ( hour === timeNow) {
+              $(this).addClass("present");
+          }
+          
+          else {
+              $(this).addClass("future");
+          }
+      })
+  }
+
+   //display current date and time in the header of the page
+  $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
+
+ timeBlocker();
 });
